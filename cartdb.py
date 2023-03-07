fc1ef4b207479050a71afe0d2501d046c66078d8
JavohirJalilov <javohirjaliloff@gmail.com>
2023-03-07 13:47:33 +0500

    project structure

---
+++ cartdb.py
@@ -0,0 +1,13 @@
+import json
+class Cart:
+    def __init__(self,cart_path:str):
+        self.cart_path = cart_path
+
+    def add(self,brand,model_id):
+        pass
+
+    def save(self):
+        pass
+
+    def get_cart(self):
+        pass
---
+++ db.py
@@ -0,0 +1,22 @@
+import json
+
+class DB:
+    def __init__(self,path):
+        self.path = path
+
+    def getPhone(self,brand,idx):
+        """
+        Return phone data by brand
+        args:
+            brand: str
+        return:
+            dict
+        """
+        pass
+
+    def get_phone_list(self,brend):
+        """
+        Return phone list
+        """
+        pass
+
