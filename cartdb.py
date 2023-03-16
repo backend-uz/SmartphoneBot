99ca528c7027c4f1efb242fe989e3b737d5b0cee
JavohirJalilov <javohirjaliloff@gmail.com>
2023-03-16 19:49:56 +0500

    card

--- cartdb.py
+++ cartdb.py
@@ -1,13 +1,30 @@
-import json
+from tinydb import TinyDB
+
 class Cart:
     def __init__(self,cart_path:str):
-        self.cart_path = cart_path
+        self.db = TinyDB(cart_path)
+        self.table = self.db.table('cart')
 
-    def add(self,brand,model_id):
-        pass
 
-    def save(self):
-        pass
+    def add(self, brand, doc_id, chat_id):
+        """
+        add card
 
-    def get_cart(self):
-        pass
+        data = {
+            'brand':brand,
+            'doc_id': doc_id,
+            chat_id: chat_id
+            }
+        """
+        data = {
+            'brand':brand,
+            'doc_id': doc_id,
+            'chat_id': chat_id
+        }
+        self.table.insert(data)
+
+    def get_cart(self, chat_id):
+        return self.table.get(chat_id=chat_id)
+    
+    def remove(self, chat_id):
+        self.table.remove(chat_id=chat_id)
