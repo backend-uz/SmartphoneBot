4c39afb6395e63e71d4cafdf55314f10e1b8ae25
backen-uz <backenduz@gmail.com>
2023-04-26 21:09:20 +0500

    connect to api

--- db.py
+++ db.py
@@ -1,14 +1,17 @@
 from tinydb import TinyDB, Query
+import requests
 
 class DB:
     def __init__(self,path):
         self.db = TinyDB(path)
+        self.base_url = "https://bacefap.pythonanywhere.com"
 
     def get_tables(self):
         """
         To get the list of all the tables in the database
         """
-        return list(self.db.tables())
+        response = requests.get(self.base_url + "/smartphone/brands")
+        return response.json()
         
     def getPhone(self,brand,idx):
         """
@@ -18,18 +21,15 @@ class DB:
         return:
             dict
         """
-        table = self.db.table(brand)
-        return table.get(doc_id=idx)
+        response = requests.get(self.base_url + "/smartphone/" + brand + "/" + str(idx))
+        return response.json()
 
     def get_phone_list(self,brand):
         """
         Return phone list
         """
-        table = self.db.table(brand)
-        return table.all()
-
-        
+        response = requests.get(self.base_url + '/smartphone/' + brand)
+        return response.json()
 
 # db = DB('db.json')
-# tables = db.get_tables()
-# print(db.get_phone_list(tables[0]))
+# print(db.get_phone_list('Apple'))
